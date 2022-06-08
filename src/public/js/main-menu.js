4b1659586149d202a1dc2b2416b7ebc7d484de78
Estefano Delgado <fanodev310@gmail.com>
2022-06-08 04:25:32 -0400

    menu responsive con background video

--- src/public/js/main-menu.js
+++ src/public/js/main-menu.js
@@ -1,8 +1,13 @@
 // DOM menu
 const menuBtn = document.querySelector(".menu-btn") 
+// DOM navigation
+const navigationBtn = document.querySelector(".navigation")
+ 
 // Event click del btn menu 
 menuBtn.addEventListener("click", () => {
+    
     menuBtn.classList.toggle("active")
+    navigationBtn.classList.toggle("active")
 
 })
 
