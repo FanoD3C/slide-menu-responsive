8913b5a8e34087a3135439368e085eef86a137c8
Estefano Delgado <fanodev310@gmail.com>
2022-06-08 03:52:39 -0400

    agregando menu resposive falta completar logica con js y sus respectivo toggles del menu

---
+++ src/public/js/main-menu.js
@@ -0,0 +1,8 @@
+// DOM menu
+const menuBtn = document.querySelector(".menu-btn") 
+// Event click del btn menu 
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active")
+
+})
+
