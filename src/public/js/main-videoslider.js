25e26c1bccf9a03598f757d34b365c606d591691
Estefano Delgado <fanodev310@gmail.com>
2022-06-09 04:23:01 -0400

    slider background temrinado falta comentar partes del codigo

---
+++ src/public/js/main-videoslider.js
@@ -0,0 +1,32 @@
+const btns = document.querySelectorAll(".nav-btn")
+const contentBtn = document.querySelectorAll(".content")
+const slidesBtn = document.querySelectorAll(".video-slide")
+
+
+
+let sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active")
+
+    });
+
+    slidesBtn.forEach((slides) => {
+        slides.classList.remove("active")
+
+    });
+    
+    contentBtn.forEach((contents) => {
+        contents.classList.remove("active")
+
+    });
+    btns[manual].classList.add("active");
+    slidesBtn[manual].classList.add("active");
+    contentBtn[manual].classList.add("active");
+};
+
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+       sliderNav(i); 
+    })
+})
